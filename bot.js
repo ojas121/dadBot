d98254590c4c00ae5959f2cf024522d715f58a41
Ojas Mandlekar <ojas.m@hotmail.com>
2021-04-07 16:35:53 +0200

    bugfix for lucas

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         if (!shutUp[channelID] || new Date() > shutUp[channelID]) {
             
-            if (message.author.id === 700733329175085058) {
+            if (message.userID === "700733329175085058") {
                 bot.sendMessage({
                     to: channelID, 
                     message: "Get HOI4 Marco", 
